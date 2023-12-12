3be90f4b941adc51a2936e6fdca827ad032ddf22
born <samuelborn@outlook.de>
2023-12-12 18:01:04 +0100

    pipe curled toolbox into tar

--- 3-main.sh
+++ 3-main.sh
@@ -56,10 +56,9 @@ cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard
 xdg-open "https://github.com/settings/keys"
 
 # JetBrains Toolbox
-wget -O jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
-tar -xf jetbrains-toolbox.tar.gz
+curl -L jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA" | tar -xz
 jetbrains-toolbox-*/jetbrains-toolbox
-rm -rf jetbrains-toolbox*
+rm -rf jetbrains-toolbox-*
 
 # Theme GTK 3 apps
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
