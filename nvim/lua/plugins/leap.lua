3c4d36b3835f5f66e2187af21d32044f4ab14626
born <samuelborn@outlook.de>
2024-01-21 10:01:21 +0100

    remove lazyload with "keys"

--- nvim/lua/plugins/leap.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "ggandor/leap.nvim",
-    opts = {},
-    keys = {
-        { "s", mode = "n", function() require('leap').leap({ target_windows = { vim.api.nvim_get_current_win() } }) end},
-    },
-}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,19 +6,18 @@ return {
     },
     config = function()
         require("telescope").load_extension("fzf")
-        require("telescope").setup { pickers = { help_tags = { mappings = { i = { ["<CR>"] = "file_vsplit" } } } } }
+        require("telescope").setup({ pickers = { help_tags = { mappings = { i = { ["<CR>"] = "file_vsplit" } } } } })
 
         vim.keymap.set("n", "<leader>ff", function() require("telescope.builtin").find_files() end)
         vim.keymap.set("n", "<leader>fb", function() require("telescope.builtin").buffers() end)
         vim.keymap.set("n", "<leader>fg", function() require("telescope.builtin").live_grep() end)
         vim.keymap.set("n", "<leader>fh", function() require("telescope.builtin").help_tags() end)
-        vim.keymap.set("n", "<leader>fe",
-            function()
-                require("telescope.builtin").find_files({
-                    cwd = "~",
-                    search_dirs = { "~/Repos/", "~/Dropbox/" },
-                    file_ignore_patterns = { "venv", "%.docx", "%.pyi", "%.pyc", "%.png", "%.jpg", "%.pdf", "%.svg", "%.note" },
-                })
-            end)
+        vim.keymap.set("n", "<leader>fe", function()
+            require("telescope.builtin").find_files({
+                cwd = "~",
+                search_dirs = { "~/Repos/", "~/Dropbox/" },
+                file_ignore_patterns = { "venv", ".venv", "%.docx", "%.pyi", "%.pyc", "%.png", "%.jpg", "%.pdf", "%.svg", "%.note" },
+            })
+        end)
     end,
 }
