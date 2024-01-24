99156438c7b585bc1f80e6cff33008ef1129c477
born <samuelborn@outlook.de>
2024-01-24 17:05:07 +0100

    update flatpak list

--- 2-flatpak.sh
+++ 2-flatpak.sh
@@ -1,30 +1,28 @@
 flatpak config --set extra-languages "de"
 
-# Regenerate list: flatpak list --app --columns=application | sed 's/$/ \\/' | sort 
+# Regenerate list: flatpak list --app --columns=application | sed 's/$/ \\/' | sort
 flatpak install -y \
-org.gtk.Gtk3theme.adw-gtk3 \
-org.gtk.Gtk3theme.adw-gtk3-dark \
-\
-com.belmoussaoui.Obfuscate \
-com.discordapp.Discord \
-com.dropbox.Client \
-com.github.finefindus.eyedropper \
-com.github.flxzt.rnote \
-com.github.jeromerobert.pdfarranger \
-com.github.johnfactotum.Foliate \
-com.github.micahflee.torbrowser-launcher \
-com.github.tchx84.Flatseal \
-com.google.Chrome \
-com.mattjakeman.ExtensionManager \
-com.spotify.Client \
-com.ticktick.TickTick \
-io.github.celluloid_player.Celluloid \
-io.github.mimbrero.WhatsAppDesktop \
-md.obsidian.Obsidian \
-net.ankiweb.Anki \
-org.gimp.GIMP \
-org.gnome.Geary \
-org.gnome.SoundRecorder \
-org.kde.okular \
-org.signal.Signal
-
+	org.gtk.Gtk3theme.adw-gtk3 \
+	org.gtk.Gtk3theme.adw-gtk3-dark \
+	\
+	com.discordapp.Discord \
+	com.dropbox.Client \
+	com.github.eneshecan.WhatsAppForLinux \
+	com.github.flxzt.rnote \
+	com.github.jeromerobert.pdfarranger \
+	com.github.johnfactotum.Foliate \
+	com.github.tchx84.Flatseal \
+	com.google.Chrome \
+	com.mattjakeman.ExtensionManager \
+	com.mojang.Minecraft \
+	com.nextcloud.desktopclient.nextcloud \
+	com.spotify.Client \
+	io.github.celluloid_player.Celluloid \
+	io.gitlab.librewolf-community \
+	md.obsidian.Obsidian \
+	net.ankiweb.Anki \
+	org.gimp.GIMP \
+	org.gnome.Geary \
+	org.kde.okular \
+	org.signal.Signal \
+	org.torproject.torbrowser-launcher
