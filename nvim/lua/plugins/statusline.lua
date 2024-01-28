652cd3e2528bfd2fdbb8380d4635b9226e0de13b
born <samuelborn@outlook.de>
2024-01-28 16:33:00 +0100

    improve statusline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -5,7 +5,9 @@ return {
             icons_enabled = false,
         },
         sections = {
-            lualine_c = { { 'filename', path = 3 } }
+            lualine_c = { { 'filename', path = 3 } },
+            lualine_x = {},
+            lualine_z = { function() return string.format("%d ln", vim.api.nvim_buf_line_count(0)) end }
         },
     },
 }
