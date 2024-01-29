0d969dddd03c010f9ab66f34730cc8e9004828d9
born <samuelborn@outlook.de>
2024-01-29 13:48:48 +0100

    add nvim surround

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 -- one-line plugin imports
 return {
-    { 'numToStr/Comment.nvim', opts = {} },
+    { 'numToStr/Comment.nvim',  opts = {} },
     { 'github/copilot.vim' },
+    { "kylechui/nvim-surround", opts = {} },
 }
