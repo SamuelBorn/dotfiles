3ebfc389bda3c41af9527380b8df5e8df0aa585e
born <samuelborn@outlook.de>
2024-01-31 13:33:17 +0100

    use keymaps that work in all modes with vim.cmd

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -7,16 +7,16 @@ vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>h", vim.cmd.split)
 
 -- Move to window using the <ctrl>+hjkl
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set({ "n", "t" }, "<C-h>", "<cmd>wincmd h<cr>")
+vim.keymap.set({ "n", "t" }, "<C-j>", "<cmd>wincmd j<cr>")
+vim.keymap.set({ "n", "t" }, "<C-k>", "<cmd>wincmd k<cr>")
+vim.keymap.set({ "n", "t" }, "<C-l>", "<cmd>wincmd l<cr>")
 
 -- Resize window using <ctrl> arrow keys
-vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>")
-vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>")
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>")
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>")
+vim.keymap.set({ "n", "t" }, "<C-Up>", "<cmd>resize +2<cr>")
+vim.keymap.set({ "n", "t" }, "<C-Down>", "<cmd>resize -2<cr>")
+vim.keymap.set({ "n", "t" }, "<C-Left>", "<cmd>vertical resize -2<cr>")
+vim.keymap.set({ "n", "t" }, "<C-Right>", "<cmd>vertical resize +2<cr>")
 
 -- Go to complete end and beginnng of file
 vim.keymap.set({ "n", "v" }, "gg", "gg0")
@@ -37,3 +37,6 @@ vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>")
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+
+-- leave terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
