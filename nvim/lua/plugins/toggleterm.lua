8d7907007bdacf85019908893733c2e9ca63d64b
born <samuelborn@outlook.de>
2024-01-31 13:32:46 +0100

    use toggleterm vertical layout

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
-        require("toggleterm").setup({ direction = "float" })
+        require("toggleterm").setup({ direction = "vertical", size = vim.o.columns * 0.4, shade_terminals = false })
 
         Lazygit = require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true })
         vim.keymap.set({ "n", "t" }, "<A-g>", "<cmd>lua Lazygit:toggle()<CR>")
