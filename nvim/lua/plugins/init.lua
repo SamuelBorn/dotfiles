8a67f884498f7c56ccf9aba50689774987db7b51
born <samuelborn@outlook.de>
2024-01-31 13:33:27 +0100

    change copilot key

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,12 @@
 -- one-line plugin imports
 return {
     { 'numToStr/Comment.nvim',  opts = {} },
-    { 'github/copilot.vim' },
     { "kylechui/nvim-surround", opts = {} },
+    {
+        'github/copilot.vim',
+        config = function()
+            vim.keymap.set('i', '<c-space>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+            vim.g.copilot_no_tab_map = true
+        end
+    },
 }
