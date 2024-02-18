47175a85fa1bfca4bb6d11d12c145211a4d79c49
Samuel Born <samuelborn@outlook.de>
2024-02-18 13:24:41 +0100

    switch toggleterm keymaps

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return {
         require("toggleterm").setup({ direction = "vertical", size = vim.o.columns * 0.4, shade_terminals = false })
 
         Lazygit = require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true })
-        vim.keymap.set({ "n", "t" }, "<A-g>", "<cmd>lua Lazygit:toggle()<CR>")
-        vim.keymap.set({ "n", "t" }, "<A-t>", "<cmd>ToggleTerm<CR>")
+        vim.keymap.set({ "n", "t" }, "<C-g>", "<cmd>lua Lazygit:toggle()<CR>")
+        vim.keymap.set({ "n", "t" }, "<C-\\>", "<cmd>ToggleTerm<CR>")
     end
 }
