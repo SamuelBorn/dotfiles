48209e9059dd83315ea2ac51be620becab0ecab7
Samuel Born <samuelborn@outlook.de>
2024-02-22 08:28:02 +0100

    make git toggleterm float

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require("toggleterm").setup({ direction = "vertical", size = vim.o.columns * 0.4, shade_terminals = false })
 
-        Lazygit = require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true })
+        Lazygit = require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true, direction="float" })
         vim.keymap.set({ "n", "t" }, "<C-g>", "<cmd>lua Lazygit:toggle()<CR>")
         vim.keymap.set({ "n", "t" }, "<C-\\>", "<cmd>ToggleTerm<CR>")
     end
