2caa01ff17b27c6ac99ad22cfc539493e133f24b
Samuel Born <samuelborn@outlook.de>
2024-02-22 08:47:57 +0100

    auto install language servers

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -27,7 +27,18 @@ return {
 
         require("neodev").setup()
         require("mason").setup()
-        require("mason-lspconfig").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "clangd",
+                "html",
+                "jsonls",
+                "lua_ls",
+                "pyright",
+                "rust_analyzer",
+                "tsserver",
+                "yamlls",
+            },
+        })
         require("mason-lspconfig").setup_handlers({
             function(server_name)
                 require("lspconfig")[server_name].setup({
