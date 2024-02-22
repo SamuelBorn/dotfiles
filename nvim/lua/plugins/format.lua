5cf0ca4e7305601b4ce95f169846f30129a805f9
Samuel Born <samuelborn@outlook.de>
2024-02-22 18:39:35 +0100

    feat: install formatters automatically

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,14 +1,15 @@
 return {
     "stevearc/conform.nvim",
+    dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
     config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                python = { "isort", "black" },
-                sh = { "shfmt" },
-                markdown = { "mdformat" },
-                javascript = { { "prettierd", "prettier" } },
-            },
-        })
+        local formatters_by_ft = {
+            javascript = { "prettierd" },
+            markdown = { "mdformat" },
+            python = { "isort", "black" },
+            sh = { "shfmt" },
+        }
+        require('mason-tool-installer').setup { ensure_installed = vim.tbl_flatten(vim.tbl_values(formatters_by_ft)) }
+        require("conform").setup({ formatters_by_ft = formatters_by_ft })
         vim.keymap.set("n", "<leader>l", function() require('conform').format({ lsp_fallback = true }) end)
     end
 }
