6a154db42db9e21bba363e20d5112b45f0ddec8a
Samuel Born <samuelborn@outlook.de>
2024-03-19 20:58:26 +0100

    add default tresitter languages

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
         require("nvim-treesitter.configs").setup {
             highlight = { enable = true },
             indent = { enable = true },
+            ensure_installed = { 'bash', 'c', 'cpp', 'rust', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'yaml' },
             auto_install = true,
             incremental_selection = {
                 enable = true,
