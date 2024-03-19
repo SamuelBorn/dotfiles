12681b8052b83c85b6a773926c8e641cf96d4798
Samuel Born <samuelborn@outlook.de>
2024-03-19 20:30:05 +0100

    add mason-conform

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,15 +1,16 @@
 return {
     "stevearc/conform.nvim",
-    dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+    dependencies = { "zapling/mason-conform.nvim" },
     config = function()
-        local formatters_by_ft = {
-            javascript = { {"prettierd", "prettier"} },
-            markdown = { "mdformat" },
-            python = { "isort", "black" },
-            sh = { "shfmt" },
-        }
-        require('mason-tool-installer').setup { ensure_installed = vim.tbl_flatten(vim.tbl_values(formatters_by_ft)) }
-        require("conform").setup({ formatters_by_ft = formatters_by_ft })
+        require("conform").setup({
+            formatters_by_ft = {
+                javascript = { { "prettierd", "prettier" } },
+                markdown = { "mdformat" },
+                python = { "isort", "black" },
+                sh = { "shfmt" },
+            }
+        })
         vim.keymap.set("n", "<leader>l", function() require('conform').format({ lsp_fallback = true }) end)
+        require("mason-conform").setup()
     end
 }
