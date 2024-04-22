47d9e917f5dd12201786ae4dda137f8222d23059
Samuel Born <samuelborn@outlook.de>
2024-04-22 10:12:27 +0200

    toggleterm cleanup

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,9 +2,9 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
-        require("toggleterm").setup({ direction = "vertical", size = vim.o.columns * 0.4, shade_terminals = false })
+        require("toggleterm").setup({ direction = "vertical", size = vim.o.columns * 0.4 })
 
-        Lazygit = require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true, direction="float" })
+        Lazygit = require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
         vim.keymap.set({ "n", "t" }, "<C-g>", "<cmd>lua Lazygit:toggle()<CR>")
         vim.keymap.set({ "n", "t" }, "<C-\\>", "<cmd>ToggleTerm<CR>")
     end
