fa32a4a49027220b00908694d139e6377693d628
Samuel Born <samuelborn@outlook.de>
2024-04-29 20:37:35 +0200

    telescope buffer better shortcut

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -7,15 +7,16 @@ return {
     config = function()
         require("telescope").load_extension("fzf")
 
+        vim.keymap.set("n", "<leader><leader>", require("telescope.builtin").buffers)
         vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
-        vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers)
         vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
         vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
         vim.keymap.set("n", "<leader>fe", function()
             require("telescope.builtin").find_files({
                 cwd = "~",
-                search_dirs = { "~/Repos/", "~/Dropbox/" },
+                search_dirs = { "~/Repos/", "~/Nextcloud/" },
                 file_ignore_patterns = { "venv", ".venv", "%.docx", "%.pyi", "%.pyc", "%.png", "%.jpg", "%.pdf", "%.svg", "%.note" },
+                hidden = true,
             })
         end)
     end,
