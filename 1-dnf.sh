91fbd54aece696cc8d5413f1a29972ca4fdd5377
Samuel Born <samuelborn@outlook.de>
2024-04-30 12:49:50 +0200

    improve dnf script

--- 1-dnf.sh
+++ 1-dnf.sh
@@ -1,41 +1,45 @@
 echo 'defaultyes=True' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=4' | sudo tee -a /etc/dnf/dnf.conf
 
-# TUI based git client
-sudo dnf copr enable atim/lazygit -y
-
 # Enable RPM Fusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager --enable fedora-cisco-openh264
 
+# TUI based git client
+sudo dnf copr enable atim/lazygit -y
+
 # Updates
 sudo dnf upgrade -y
 
+# Nvidia and Multimedia Configuration
+sudo dnf install akmod-nvidia nvidia-vaapi-driver
+sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
+
+# Useful packages
 sudo dnf install -y \
-    adw-gtk3-theme \
-    akmod-nvidia \
-    cmake \
-    ffmpeg \
-    flameshot \
-    g++ \ 
-    gcc \
-    gnome-pomodoro \
-    gnome-tweaks \
-    htop \
-    hunspell-de \
-    lazygit \
-    neofetch \
-    neovim \
-    nodejs \ 
-    ripgrep \
-    stow \
-    tldr \
-    trash-cli \
-    util-linux \
-    xclip \
-    zoxide \
-    zsh \
-    zsh-autosuggestions \
-    zsh-syntax-highlighting
+	adw-gtk3-theme \
+	cmake \
+	ffmpeg \
+	flameshot \
+	g++ \
+	gcc \
+	gnome-pomodoro \
+	gnome-tweaks \
+	htop \
+	hunspell-de \
+	lazygit \
+	neofetch \
+	neovim \
+	nodejs \
+	ripgrep \
+	stow \
+	tldr \
+	trash-cli \
+	util-linux \
+	xclip \
+	zoxide \
+	zsh \
+	zsh-autosuggestions \
+	zsh-syntax-highlighting
 
 reboot
