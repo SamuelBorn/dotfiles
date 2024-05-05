bc12adb98b7cad5d047c11dbf3585aad74b8d5a6
Samuel Born <samuelborn@outlook.de>
2024-05-05 09:16:51 +0200

    add smart search

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -2,12 +2,14 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        { "danielfalk/smart-open.nvim",               dependencies = "kkharji/sqlite.lua" },
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
         require("telescope").load_extension("fzf")
+        require("telescope").load_extension("smart_open")
 
-        vim.keymap.set("n", "<leader><leader>", require("telescope.builtin").buffers)
+        vim.keymap.set("n", "<leader><leader>", require("telescope").extensions.smart_open.smart_open)
         vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
         vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
         vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
