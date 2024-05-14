ae0ea9a6adabdd677a89bf2fea4045dc9ad74d0b
Samuel Born <samuelborn@outlook.de>
2024-05-14 21:23:56 +0200

    remove esc keymap in lazygit

--- home/.config/nvim/lua/plugins/toggleterm.lua
+++ home/.config/nvim/lua/plugins/toggleterm.lua
@@ -4,7 +4,15 @@ return {
     config = function()
         require("toggleterm").setup({ direction = "vertical", size = vim.o.columns * 0.4 })
 
-        Lazygit = require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+        Lazygit = require("toggleterm.terminal").Terminal:new({
+            cmd = "lazygit",
+            hidden = true,
+            direction = "float",
+            on_open = function(term)
+                -- reset Esc key to leave terminal mode so it is usable in lazygit
+                vim.keymap.set("t", "<Esc>", "<Esc>", { buffer = term.bufnr })
+            end,
+        })
         vim.keymap.set({ "n", "t" }, "<C-g>", "<cmd>lua Lazygit:toggle()<CR>")
         vim.keymap.set({ "n", "t" }, "<C-\\>", "<cmd>ToggleTerm<CR>")
     end
