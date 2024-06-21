998d4c31c2e22ee2fcd0eff04543edca891f0819
Samuel Born <samuelborn@outlook.de>
2024-06-21 09:30:16 +0200

    use ff for global grep

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -10,15 +10,7 @@ return {
         require("telescope").load_extension("smart_open")
 
         vim.keymap.set("n", "<leader><leader>", require("telescope").extensions.smart_open.smart_open)
-        vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
+        vim.keymap.set("n", "<leader>ff", require("telescope.builtin").live_grep)
         vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
-        vim.keymap.set("n", "<leader>fe", function()
-            require("telescope.builtin").find_files({
-                cwd = "~",
-                search_dirs = { "~/Repos/", "~/Nextcloud/" },
-                file_ignore_patterns = { "venv", ".venv", "%.docx", "%.pyi", "%.pyc", "%.png", "%.jpg", "%.pdf", "%.svg", "%.note" },
-                hidden = true,
-            })
-        end)
     end,
 }
