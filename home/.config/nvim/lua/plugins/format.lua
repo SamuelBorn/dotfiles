324e8ed0a7d1dd9a3c386d36671e50c1f15f1477
Samuel Born <samuelborn@outlook.de>
2024-06-22 09:17:42 +0200

    add nvim-lspconfig to depencies of conform so format lsp fallback works

--- home/.config/nvim/lua/plugins/format.lua
+++ home/.config/nvim/lua/plugins/format.lua
@@ -1,6 +1,6 @@
 return {
     "stevearc/conform.nvim",
-    dependencies = { "zapling/mason-conform.nvim" },
+    dependencies = { "zapling/mason-conform.nvim", "neovim/nvim-lspconfig" },
     config = function()
         require("conform").setup({
             formatters_by_ft = {
