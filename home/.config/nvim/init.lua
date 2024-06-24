f566996ccb2699b02cf91988f75a5b7ed0cdd1b8
Samuel Born <samuelborn@outlook.de>
2024-06-24 16:15:27 +0200

    add autocmd to restore cursor

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,2 +1,3 @@
+require("autocmds")
 require("config")
 require("keymaps")
---
+++ home/.config/nvim/lua/autocmds.lua
@@ -0,0 +1,11 @@
+vim.cmd([[
+    augroup RestoreCursor
+      autocmd!
+      autocmd BufRead * autocmd FileType <buffer> ++once
+        \ let s:line = line("'\"")
+        \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
+        \      && index(['xxd', 'gitrebase'], &filetype) == -1
+        \ |   execute "normal! g`\""
+        \ | endif
+    augroup END
+]])
