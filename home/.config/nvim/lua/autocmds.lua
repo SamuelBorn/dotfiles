13ce91cc7900318b6a68caccb683b59b1d182c56
Samuel Born <samuelborn@outlook.de>
2024-06-26 09:25:48 +0200

    add useful autocmd

--- home/.config/nvim/lua/autocmds.lua
+++ home/.config/nvim/lua/autocmds.lua
@@ -1,12 +1,26 @@
--- Restore cursor position when opening files
-vim.cmd([[
-    augroup RestoreCursor
-      autocmd!
-      autocmd BufRead * autocmd FileType <buffer> ++once
-        \ let s:line = line("'\"")
-        \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
-        \      && index(['xxd', 'gitrebase'], &filetype) == -1
-        \ |   execute "normal! g`\""
-        \ | endif
-    augroup END
-]])
+-- check if we need to reload the file when it changed (e.g. after git reset)
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+    group = vim.api.nvim_create_augroup("checktime", {}),
+    callback = function()
+        if vim.o.buftype ~= "nofile" then
+            vim.cmd("checktime")
+        end
+    end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = vim.api.nvim_create_augroup("last_loc", {}),
+    callback = function(event)
+        local mark = vim.api.nvim_buf_get_mark(event.buf, '"')
+        pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end,
+})
+
+-- highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("highlight_yank", {}),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
