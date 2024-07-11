8a264b22d24306640ca456a94602e761c21faa8d
Samuel Born <samuelborn@outlook.de>
2024-07-11 22:30:17 +0200

    improve open last pos automc

--- home/.config/nvim/lua/autocmds.lua
+++ home/.config/nvim/lua/autocmds.lua
@@ -12,8 +12,12 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 vim.api.nvim_create_autocmd("BufReadPost", {
     group = vim.api.nvim_create_augroup("last_loc", {}),
     callback = function(event)
-        local mark = vim.api.nvim_buf_get_mark(event.buf, '"')
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        local buf = event.buf
+        local mark = vim.api.nvim_buf_get_mark(buf, '"')
+        local lcount = vim.api.nvim_buf_line_count(buf)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
     end,
 })
 
