af4d9e58e03e6ea93899baf0cec08897886f3631
Samuel Born <samuelborn@outlook.de>
2024-08-20 09:45:33 +0200

    update statusline

--- home/.config/nvim/lua/plugins/statusline.lua
+++ home/.config/nvim/lua/plugins/statusline.lua
@@ -7,9 +7,12 @@ return {
             component_separators = '',
         },
         sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'diff' },
             lualine_c = { { 'filename', path = 3 } },
             lualine_x = {},
-            lualine_z = { function() return string.format("%d ln", vim.api.nvim_buf_line_count(0)) end }
+            lualine_y = { 'filetype' },
+            lualine_z = { function() return string.format("%d ln", vim.api.nvim_buf_line_count(0)) end },
         },
     },
 }
