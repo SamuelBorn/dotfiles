1528f0606bbb80e6a5d4fdde8765047b22dd621d
Samuel Born <samuelborn@outlook.de>
2024-08-26 15:19:57 +0200

    use kitty over gnome-terminal

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,12 @@
 return {
-    "ellisonleao/gruvbox.nvim",
+    -- "folke/tokyonight.nvim",
+    -- "ellisonleao/gruvbox.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("gruvbox")
+        vim.cmd.colorscheme("catppuccin")
+        -- vim.cmd.colorscheme("tokyonight-night")
+        -- vim.cmd.colorscheme("gruvbox")
     end,
 }
