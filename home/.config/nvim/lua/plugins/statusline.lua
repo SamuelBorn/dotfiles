22eb761d8768c40d75d2570785d9e1ced5ec6190
Samuel Born <samuelborn@outlook.de>
2024-08-27 13:57:19 +0200

    improve statusline

--- home/.config/nvim/lua/plugins/statusline.lua
+++ home/.config/nvim/lua/plugins/statusline.lua
@@ -8,9 +8,9 @@ return {
             lualine_a = { 'mode' },
             lualine_b = { 'diff' },
             lualine_c = { { 'filename', path = 3 } },
-            lualine_x = {},
-            lualine_y = { 'filetype' },
-            lualine_z = { function() return string.format("%d ln", vim.api.nvim_buf_line_count(0)) end },
+            lualine_x = { 'filetype' },
+            lualine_y = { function() return string.format("%d ln", vim.api.nvim_buf_line_count(0)) end },
+            lualine_z = {},
         },
     },
 }
