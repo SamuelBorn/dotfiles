142a012a343dc1b4c0d48c221c3ab7760e3686c2
Samuel Born <samuelborn@outlook.de>
2024-08-27 11:27:37 +0200

    change theme

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -25,6 +25,11 @@ return {
         "projekt0n/github-nvim-theme",
         priority = 1000,
         config = function()
+            require("github-theme").setup({
+                options = {
+                    transparent = true,
+                },
+            })
             vim.cmd.colorscheme("github_dark_default")
         end
     },
