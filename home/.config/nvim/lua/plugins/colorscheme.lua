6bb5e3dffba5874c0865af3b2974f4e6605aedad
Samuel Born <samuelborn@outlook.de>
2024-09-12 10:41:40 +0200

    change theme

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,35 +5,9 @@ return {
         priority = 1000,
         config = function()
             require("catppuccin").setup({
-                -- transparent_background = true,
+                transparent_background = true,
             })
-            -- vim.cmd.colorscheme("catppuccin")
-        end
-    },
-    {
-        "folke/tokyonight.nvim",
-        priority = 1000,
-        config = function()
-            -- vim.cmd.colorscheme("tokyonight-night")
-        end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("gruvbox")
-        end
-    },
-    {
-        "projekt0n/github-nvim-theme",
-        priority = 1000,
-        config = function()
-            require("github-theme").setup({
-                options = {
-                    -- transparent = true,
-                },
-            })
-            -- vim.cmd.colorscheme("github_dark_default")
+            vim.cmd.colorscheme("catppuccin-frappe")
         end
     },
 }
