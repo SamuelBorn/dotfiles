a3ba811cba97ec29e99ba127e9722a6d88b74d72
Samuel Born <samuelborn@outlook.de>
2024-09-21 09:57:03 +0200

    use lazy keys for oil setup

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -1,12 +1,9 @@
 return {
-	{ "echasnovski/mini.surround", opts = {}, event = "VeryLazy" },
-	{ "github/copilot.vim", event = "VeryLazy" },
-	{
-		"stevearc/oil.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("oil").setup()
-			vim.keymap.set("n", "-", "<cmd>Oil<cr>")
-		end,
-	},
+    { "echasnovski/mini.surround", event = "VeryLazy", opts = {} },
+    { "github/copilot.vim",        event = "VeryLazy" },
+    {
+        "stevearc/oil.nvim",
+        opts = {},
+        keys = { { "-", "<cmd>Oil<cr>" } },
+    },
 }
