156cd0db9cb39415cff1c6bd426ab6b552921ed7
Samuel Born <samuelborn@outlook.de>
2024-10-16 11:44:50 +0200

    change color theme

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,9 +5,24 @@ return {
         priority = 1000,
         config = function()
             require("catppuccin").setup({
-                transparent_background = true,
+                -- transparent_background = true,
             })
-            vim.cmd.colorscheme("catppuccin-frappe")
+            -- vim.cmd.colorscheme("catppuccin-mocha")
+        end
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            -- vim.cmd.colorscheme("gruvbox")
+        end
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("tokyonight-night")
         end
     },
 }
