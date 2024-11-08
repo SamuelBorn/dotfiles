6e9c316d163fe9d61f3e26edb13b556b4808aae6
Samuel Born <samuelborn@outlook.de>
2024-11-08 10:58:10 +0100

    clean up keymaps

--- home/.config/nvim/lua/keymaps.lua
+++ home/.config/nvim/lua/keymaps.lua
@@ -1,34 +1,18 @@
--- Useful custom keymaps
-vim.keymap.set("n", "<leader>w", "<cmd>wa<cr>")
+-- Save and quit
+vim.keymap.set("n", "<leader>w", vim.cmd.write)
 vim.keymap.set("n", "<leader>q", vim.cmd.quit)
 vim.keymap.set("n", "<leader>o", vim.cmd.only)
-
--- Split window
-vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
-vim.keymap.set("n", "<leader>h", vim.cmd.split)
-
--- Move to window using the <ctrl>+hjkl
-vim.keymap.set({ "n", "t" }, "<C-h>", "<cmd>wincmd h<cr>")
-vim.keymap.set({ "n", "t" }, "<C-j>", "<cmd>wincmd j<cr>")
-vim.keymap.set({ "n", "t" }, "<C-k>", "<cmd>wincmd k<cr>")
-vim.keymap.set({ "n", "t" }, "<C-l>", "<cmd>wincmd l<cr>")
-
--- Resize window using <ctrl> arrow keys
-vim.keymap.set({ "n", "t" }, "<C-Up>", "<cmd>resize +2<cr>")
-vim.keymap.set({ "n", "t" }, "<C-Down>", "<cmd>resize -2<cr>")
-vim.keymap.set({ "n", "t" }, "<C-Left>", "<cmd>vertical resize -2<cr>")
-vim.keymap.set({ "n", "t" }, "<C-Right>", "<cmd>vertical resize +2<cr>")
 
 -- Go to complete end and beginnng of file
 vim.keymap.set({ "n", "x" }, "gg", "gg0")
 vim.keymap.set({ "n", "x" }, "G", "G$")
 
+-- Select all
+vim.keymap.set({ "n", "x" }, "<C-a>", "<Esc>ggVG")
+
 -- Yank to system clipboard
 vim.keymap.set({ "n", "x" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+y$')
-
--- Delete without overriding nvim clipboard
-vim.keymap.set({ "n", "x" }, "<leader>d", '"_d')
 
 -- Leave terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
@@ -39,13 +23,14 @@ vim.keymap.set("c", "w!!", "w !sudo tee % > /dev/null")
 -- Unhighlight search
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Remove annoying keymaps
+-- Remove annoying keymap
 vim.keymap.set("n", "q:", ":q")
 
--- Select all
-vim.keymap.set({ "i", "n", "x" }, "<C-a>", "<Esc>ggVG")
+-- Move lines up and down
+vim.keymap.set("x", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("x", "K", ":m '<-2<cr>gv=gv")
 
--- Better j k in wrapped lines
+-- Better j k in wrapped lines (10j still behaves like j)
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
@@ -54,3 +39,15 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- Window management
+vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>h", vim.cmd.split)
+vim.keymap.set({ "n", "t" }, "<C-h>", "<cmd>wincmd h<cr>")
+vim.keymap.set({ "n", "t" }, "<C-j>", "<cmd>wincmd j<cr>")
+vim.keymap.set({ "n", "t" }, "<C-k>", "<cmd>wincmd k<cr>")
+vim.keymap.set({ "n", "t" }, "<C-l>", "<cmd>wincmd l<cr>")
+vim.keymap.set({ "n", "t" }, "<C-Up>", "<cmd>resize +2<cr>")
+vim.keymap.set({ "n", "t" }, "<C-Down>", "<cmd>resize -2<cr>")
+vim.keymap.set({ "n", "t" }, "<C-Left>", "<cmd>vertical resize -2<cr>")
+vim.keymap.set({ "n", "t" }, "<C-Right>", "<cmd>vertical resize +2<cr>")
