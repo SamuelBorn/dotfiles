574efdbc83477aa740cfb40b08b002cc1cfe3563
Samuel Born <samuelborn@outlook.de>
2024-11-10 23:14:19 +0100

    improve telescope config

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -8,7 +8,6 @@ return {
     },
     -- {
     --     "folke/tokyonight.nvim",
-    --     lazy = false,
     --     priority = 1000,
     --     config = function()
     --         vim.cmd.colorscheme("tokyonight-night")
--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -2,18 +2,19 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
-        { "danielfalk/smart-open.nvim",               dependencies = "kkharji/sqlite.lua" },
+        { "danielfalk/smart-open.nvim" },
+        { "kkharji/sqlite.lua" },
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
-    event = "VeryLazy",
     config = function()
         require('telescope').setup({ extensions = { smart_open = { match_algorithm = "fzf" } } })
         require("telescope").load_extension("fzf")
         require("telescope").load_extension("smart_open")
-
-        vim.keymap.set("n", "<leader><leader>", require("telescope").extensions.smart_open.smart_open)
-        vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
-        vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
-        vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
     end,
+    keys = {
+        { "<leader><leader>", ":Telescope smart_open<CR>" },
+        { "<leader>ff",       require("telescope.builtin").find_files },
+        { "<leader>fg",       require("telescope.builtin").live_grep },
+        { "<leader>fh",       require("telescope.builtin").help_tags },
+    },
 }
