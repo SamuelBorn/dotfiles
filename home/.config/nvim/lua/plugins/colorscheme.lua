9647a0f7b512fc1f313f0e04a50c31fbd85b199d
Samuel Born <samuelborn@outlook.de>
2024-11-10 23:09:49 +0100

    clean up colorschemes

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,17 @@
 return {
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                -- transparent_background = true,
-            })
-            -- vim.cmd.colorscheme("catppuccin-mocha")
-        end
-    },
-    {
         "ellisonleao/gruvbox.nvim",
         priority = 1000,
         config = function()
             vim.cmd.colorscheme("gruvbox")
         end
     },
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- vim.cmd.colorscheme("tokyonight-night")
-        end
-    },
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd.colorscheme("tokyonight-night")
+    --     end
+    -- },
 }
