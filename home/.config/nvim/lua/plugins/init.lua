201fe63b616891510e1779d4449a54fa68f582e2
Samuel Born <samuelborn@outlook.de>
2024-12-03 09:11:48 +0100

    add copilot accept word shortcut

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -1,6 +1,12 @@
 return {
     { "echasnovski/mini.surround", event = "VeryLazy", opts = {} },
-    { "github/copilot.vim",        event = "VeryLazy" },
+    {
+        "github/copilot.vim",
+        event = "VeryLazy",
+        config = function()
+            vim.keymap.set('i', '<C-o>', '<Plug>(copilot-accept-word)')
+        end
+    },
     {
         "stevearc/oil.nvim",
         opts = {
