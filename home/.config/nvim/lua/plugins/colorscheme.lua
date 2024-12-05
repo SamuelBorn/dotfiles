6bd7c2308aae37198bda5f26ce5259cb1e8bd903
Samuel Born <samuelborn@outlook.de>
2024-12-05 10:19:03 +0100

    use gruvbox material

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
         "ellisonleao/gruvbox.nvim",
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme("gruvbox")
+            -- vim.cmd.colorscheme("gruvbox")
         end
     },
     -- {
@@ -13,4 +13,12 @@ return {
     --         vim.cmd.colorscheme("tokyonight-night")
     --     end
     -- },
+    {
+        "sainnhe/gruvbox-material",
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_material_palette = "mix"
+            vim.cmd.colorscheme("gruvbox-material")
+        end
+    }
 }
