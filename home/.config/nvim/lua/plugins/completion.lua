f846b5e2e6e00b644040fee3d45e18e90df8c4be
Samuel Born <samuelborn@outlook.de>
2024-12-09 14:08:42 +0100

    add luasnip snippets

--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -6,46 +6,27 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-buffer",
-        {
-            "rafamadriz/friendly-snippets",
-            config = function()
-                require("luasnip.loaders.from_vscode").lazy_load()
-            end
-        },
+        "rafamadriz/friendly-snippets",
     },
     event = "VeryLazy",
     config = function()
-        local ls = require("luasnip")
-        require("luasnip").config.setup()
-        ls.add_snippets("cpp", {
-            ls.snippet("cout", {
-                ls.text_node("std::cout << "),
-                ls.insert_node(1),
-                ls.text_node(" << std::endl;"),
-            }),
-        }, { override_priority = 1001 })
-
         local cmp = require("cmp")
+        local ls = require("luasnip")
+
+        ls.config.setup({ enable_autosnippets = true })
+        require("luasnip.loaders.from_lua").lazy_load({ paths = { "./lua/snippets" } })
+        require("luasnip.loaders.from_vscode").lazy_load()
+
         cmp.setup {
             snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
+                expand = function(args) ls.lsp_expand(args.body) end,
             },
             mapping = cmp.mapping.preset.insert {
                 ["<C-n>"] = cmp.mapping.select_next_item(),
                 ["<C-p>"] = cmp.mapping.select_prev_item(),
                 ["<C-y>"] = cmp.mapping.confirm { select = true },
-                ['<C-l>'] = cmp.mapping(function()
-                    if require("luasnip").expand_or_locally_jumpable() then
-                        require("luasnip").expand_or_jump()
-                    end
-                end, { 'i', 's' }),
-                ['<C-h>'] = cmp.mapping(function()
-                    if require("luasnip").locally_jumpable(-1) then
-                        require("luasnip").jump(-1)
-                    end
-                end, { 'i', 's' }), },
+                ["<C-l>"] = cmp.mapping(function() if ls.expand_or_locally_jumpable() then ls.expand_or_jump() end end, { 'i', 's' }),
+                ["<C-h>"] = cmp.mapping(function() if ls.locally_jumpable(-1) then ls.jump(-1) end end, { 'i', 's' }), },
             sources = {
                 { name = "luasnip" },
                 { name = "nvim_lsp" },
---
+++ home/.config/nvim/lua/snippets/cpp.lua
@@ -0,0 +1,15 @@
+return {}, {
+    s("cout", {t("std::cout << "), i(1), t(" << std::endl;")}),
+    postfix(".for", {
+        l("for (auto e : " .. l.POSTFIX_MATCH .. ") {"),
+        t({"", "\t"}),
+        i(0),
+        t({"", "}"}),
+    }),
+    postfix(".print", {
+        l("for (auto e : " .. l.POSTFIX_MATCH .. ") {"),
+        t({"", "\t"}),
+        t({"std::cout << e << std::endl;", "}"}),
+        i(0),
+    }),
+}
