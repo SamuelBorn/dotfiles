88934d53c4c9b8fb0cd728079c4eb3d1ba471add
Samuel Born <samuelborn@outlook.de>
2024-12-10 12:20:35 +0100

    fix syntax
    fix syntax

--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -1,12 +1,12 @@
 return {
     "hrsh7th/nvim-cmp",
     dependencies = {
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-buffer",
+        "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets",
+        "L3MON4D3/LuaSnip",
     },
     event = "VeryLazy",
     config = function()
@@ -24,7 +24,7 @@ return {
             mapping = cmp.mapping.preset.insert {
                 ["<C-n>"] = cmp.mapping.select_next_item(),
                 ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-y>"] = cmp.mapping.confirm { select = true },
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-l>"] = cmp.mapping(function() if ls.expand_or_locally_jumpable() then ls.expand_or_jump() end end, { 'i', 's' }),
                 ["<C-h>"] = cmp.mapping(function() if ls.locally_jumpable(-1) then ls.jump(-1) end end, { 'i', 's' }), },
             sources = {
