86c6d96f39e19162db3191648e6527fc62b01e7c
Samuel Born <samuelborn@outlook.de>
2024-12-13 09:55:15 +0100

    improve yank shortcut

--- home/.config/nvim/lua/keymaps.lua
+++ home/.config/nvim/lua/keymaps.lua
@@ -13,7 +13,7 @@ vim.keymap.set({ "n", "x" }, "<C-a>", "<Esc>ggVG")
 -- Yank to system clipboard
 vim.keymap.set({ "n", "x" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+y$')
-vim.keymap.set("n", "<A-y>", 'mzgg"+yG`z')
+vim.keymap.set("n", "<A-y>", '<cmd>%y+<cr>')
 
 -- Leave terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
