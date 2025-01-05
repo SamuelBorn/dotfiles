0d93e8f34ed9af0fcfc5afd3488410f14b3e7039
Samuel Born <samuelborn@outlook.de>
2025-01-05 17:07:13 +0100

    simplify completion / remove snippets

--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -1,13 +1,6 @@
 return {
     "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
     version = "*",
-    dependencies = {
-        { "rafamadriz/friendly-snippets" },
-        { "L3MON4D3/LuaSnip",            version = "*" },
-    },
-    config = function()
-        require("luasnip").config.setup({ enable_autosnippets = true })
-        require("luasnip.loaders.from_lua").lazy_load({ paths = { "./snippets" } })
-        require("blink.cmp").setup({ sources = { cmdline = {} } })
-    end,
+    opts = {},
 }
