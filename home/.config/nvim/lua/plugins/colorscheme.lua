92bbf531260a72c4f4f926c4b314300ad2711dca
Samuel Born <samuelborn@outlook.de>
2025-01-19 15:09:14 +0100

    change colorscheme

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,30 @@
 return {
-    "sainnhe/gruvbox-material",
-    priority = 1000,
-    config = function()
-        vim.g.gruvbox_material_palette = "mix"
-        vim.cmd.colorscheme("gruvbox-material")
-    end
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end
+    },
+    -- {
+    --     'projekt0n/github-nvim-theme',
+    --     priority = 1000,
+    --
+    -- },
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     priority = 1000,
+    -- },
+    -- {
+    --     "sainnhe/gruvbox-material",
+    --     priority = 1000,
+    --     config = function()
+    --         vim.g.gruvbox_material_palette = "mix"
+    --     end
+    -- },
+    -- {
+    --     "rebelot/kanagawa.nvim",
+    --     priority = 1000,
+    -- },
 }
