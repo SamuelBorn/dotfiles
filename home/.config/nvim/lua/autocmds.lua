f94ca7b1d91c1e29caa0144f6d633c847baa6dda
Samuel Born <samuelborn@outlook.de>
2025-01-31 12:25:38 +0100

    clean autocmds

--- home/.config/nvim/lua/autocmds.lua
+++ home/.config/nvim/lua/autocmds.lua
@@ -10,11 +10,10 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermLeave" }, {
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
     callback = function(event)
-        local buf = event.buf
-        local mark = vim.api.nvim_buf_get_mark(buf, '"')
-        local lcount = vim.api.nvim_buf_line_count(buf)
+        local mark = vim.api.nvim_buf_get_mark(event.buf, '"')
+        local lcount = vim.api.nvim_buf_line_count(event.buf)
         if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+            vim.api.nvim_win_set_cursor(0, mark)
         end
     end,
 })
