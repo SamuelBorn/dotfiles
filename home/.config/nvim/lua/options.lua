8e7c71674aee4b3f719a9a05257aa9552dc36c2a
Samuel Born <samuelborn@outlook.de>
2025-02-21 09:43:32 +0100

    restructure repo

--- home/.config/nvim/lua/options.lua
+++ home/.config/nvim/lua/options.lua
@@ -33,18 +33,3 @@ vim.opt.spelllang = "en_us,de_de"
 
 -- Remove the tilde at the end of the buffer
 vim.opt.fillchars = { eob = " " }
-
--- Set up lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
