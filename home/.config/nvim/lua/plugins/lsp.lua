0bd3df1196daacf73ae38e3322e46c7f9cc1995e
Samuel Born <samuelborn@outlook.de>
2025-04-16 11:47:52 +0200

    simplify lsp setup

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -4,9 +4,12 @@ return {
         { "folke/lazydev.nvim",               opts = {}, ft = "lua" },
         { "williamboman/mason.nvim",          opts = {} },
         { "williamboman/mason-lspconfig.nvim" },
-        { "saghen/blink.cmp" },
     },
     config = function()
+        vim.api.nvim_create_autocmd("CursorHold", {
+            callback = function() vim.diagnostic.open_float(nil, { focusable = false }) end
+        })
+
         vim.api.nvim_create_autocmd("LspProgress", {
             callback = function()
                 vim.notify(vim.lsp.status(), vim.log.levels.TRACE, {
@@ -17,7 +20,7 @@ return {
         })
 
         vim.api.nvim_create_autocmd('LspAttach', {
-            callback = function(args)
+            callback = function()
                 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
                 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
                 vim.keymap.set("n", "<leader>ds", function() Snacks.picker.lsp_workspace_symbols() end)
@@ -26,43 +29,25 @@ return {
 
                 vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help)
 
-                vim.keymap.set("n", "<leader>i", function()
-                    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-                end)
-
-                vim.diagnostic.config({ virtual_text = false })
-                vim.api.nvim_create_autocmd("CursorHold", {
-                    buffer = args.buf,
-                    callback = function()
-                        vim.diagnostic.open_float(nil, { focusable = false })
-                    end
-                })
+                vim.keymap.set("n", "<leader>i",
+                    function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
             end
         })
 
         local servers = {
-            clangd = {},
-            lua_ls = {},
-            basedpyright = {
-                settings = {
-                    basedpyright = {
-                        analysis = {
-                            typeCheckingMode = "standard",
-                        }
-                    }
-                }
-            },
-            ruff = {},
-            rust_analyzer = {},
-            texlab = {},
-            jdtls = {},
+            "clangd",
+            "lua_ls",
+            "pyright",
+            "ruff",
+            "rust_analyzer",
+            "texlab",
+            "jdtls",
         }
 
-        require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(servers) })
+        require("mason-lspconfig").setup({ ensure_installed = servers })
 
-        for server, config in pairs(servers) do
-            config.capabilities = require('blink.cmp').get_lsp_capabilities()
-            require("lspconfig")[server].setup(config)
+        for _, server in ipairs(servers) do
+            vim.lsp.enable(server)
         end
     end,
 }
