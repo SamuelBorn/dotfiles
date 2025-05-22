70af66bc10bab9a5e29e2ad5757c0750708d4053
Samuel Born <samuelborn@outlook.de>
2025-05-22 09:34:43 +0200

    improve mason config

--- home/.config/nvim/lua/plugins/format.lua
+++ home/.config/nvim/lua/plugins/format.lua
@@ -1,23 +1,18 @@
 return {
     "stevearc/conform.nvim",
-    dependencies = { "zapling/mason-conform.nvim" },
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                javascript = { "prettierd" },
-                json       = { "prettierd" },
-                markdown   = { "prettierd" },
-                html       = { "prettierd" },
-                css        = { "prettierd" },
-                sh         = { "shfmt" },
-                zsh        = { "beautysh" },
-                tex        = { "latexindent" },
-                cmake      = { "cmake_format" },
-            },
-        })
-        require("mason-conform").setup()
-    end,
-    keys = {
-        { "<leader>l", function() require("conform").format({ lsp_format = "fallback" }) end },
+    dependencies = { "zapling/mason-conform.nvim", opts = {} },
+    opts = {
+        formatters_by_ft = {
+            javascript = { "prettierd" },
+            json       = { "prettierd" },
+            markdown   = { "prettierd" },
+            html       = { "prettierd" },
+            css        = { "prettierd" },
+            sh         = { "shfmt" },
+            zsh        = { "beautysh" },
+            tex        = { "latexindent" },
+            cmake      = { "cmake_format" },
+        },
     },
+    keys = { { "<leader>l", function() require("conform").format({ lsp_format = "fallback" }) end } },
 }
