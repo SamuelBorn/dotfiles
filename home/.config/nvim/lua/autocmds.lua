e2bb1ed430874eb81e3dcf309a20c2559c0bf854
Samuel Born <samuelborn@outlook.de>
2025-06-24 14:40:19 +0200

    nvim cleanup

--- home/.config/nvim/lua/autocmds.lua
+++ home/.config/nvim/lua/autocmds.lua
@@ -42,10 +42,3 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.spell = true
     end,
 })
-
--- Show errors and warnings in a floating window
-vim.api.nvim_create_autocmd("CursorHold", {
-    callback = function()
-        vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
-    end,
-})
--- home/.config/nvim/lua/keymaps.lua
+++ home/.config/nvim/lua/keymaps.lua
@@ -3,17 +3,20 @@ vim.keymap.set("n", "<leader>w", "<cmd>wa<cr>")
 vim.keymap.set("n", "<leader>q", vim.cmd.quit)
 vim.keymap.set("n", "<leader>o", vim.cmd.only)
 
+-- Buffer management
+vim.keymap.set("n", "H", "<cmd>bprevious<cr>")
+vim.keymap.set("n", "L", "<cmd>bnext<cr>")
+
 -- Select all
 vim.keymap.set("n", "<C-a>", "ggVG")
-
--- Execute lua code
-vim.keymap.set("n", "<leader>x", ":.lua<cr>")
-vim.keymap.set("x", "<leader>x", ":lua<cr>")
 
 -- Yank to system clipboard
 vim.keymap.set({ "n", "x" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+y$')
 vim.keymap.set("n", "<A-y>", '<cmd>%y+<cr>')
+
+-- Dont yank on x
+vim.keymap.set("n", "x", '"_x')
 
 -- Duplicate line and comment one
 vim.keymap.set("n", "gC", "yygccP", { remap = true })
@@ -22,7 +25,7 @@ vim.keymap.set("x", "gC", "ygvgcP", { remap = true })
 -- Save file with sudo permissions
 vim.keymap.set("ca", "w!!", "w !sudo tee % > /dev/null")
 
--- Make
+-- Write before running make
 vim.keymap.set("n", "<leader>m", "<cmd>wa | make<cr>")
 
 -- Replace text
@@ -41,15 +44,12 @@ vim.keymap.set("x", "J", "j")
 -- Toggle inlay hints
 vim.keymap.set("n", "<leader>i", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
 
--- Buffer management
-vim.keymap.set("n", "H", "<cmd>bprevious<cr>")
-vim.keymap.set("n", "L", "<cmd>bnext<cr>")
+-- Toggle diagnostics
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
 
--- Move lines up and down
-vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==")
-vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==")
-vim.keymap.set("x", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv")
-vim.keymap.set("x", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv")
+-- Execute lua code
+vim.keymap.set("n", "<leader>x", ":.lua<cr>")
+vim.keymap.set("x", "<leader>x", ":lua<cr>")
 
 -- Better j k in wrapped lines (10j still behaves like normal j)
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -60,14 +60,16 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "*", "*zz")
+vim.keymap.set("n", "#", "#zz")
 
 -- Window management
 vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>h", vim.cmd.split)
-vim.keymap.set({ "n", "t", "i" }, "<C-h>", function() vim.cmd.wincmd("h") end)
-vim.keymap.set({ "n", "t", "i" }, "<C-j>", function() vim.cmd.wincmd("j") end)
-vim.keymap.set({ "n", "t", "i" }, "<C-k>", function() vim.cmd.wincmd("k") end)
-vim.keymap.set({ "n", "t", "i" }, "<C-l>", function() vim.cmd.wincmd("l") end)
+vim.keymap.set({ "n", "t" }, "<C-h>", function() vim.cmd.wincmd("h") end)
+vim.keymap.set({ "n", "t" }, "<C-j>", function() vim.cmd.wincmd("j") end)
+vim.keymap.set({ "n", "t" }, "<C-k>", function() vim.cmd.wincmd("k") end)
+vim.keymap.set({ "n", "t" }, "<C-l>", function() vim.cmd.wincmd("l") end)
 vim.keymap.set("n", "<C-Up>", function() vim.cmd.resize("+2") end)
 vim.keymap.set("n", "<C-Down>", function() vim.cmd.resize("-2") end)
 vim.keymap.set("n", "<C-Left>", function() vim.cmd("vertical resize +2") end)
