c676491069ac403057118a74dfbcfb3227022897
Samuel Born <samuelborn@outlook.de>
2025-08-11 11:55:04 +0200

    clean nvim config

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,17 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup {
-                color_overrides = {
-                    mocha = {
-                        base = "#1c1b22",
-                        crust = "#1c1b22",
-                        mantle = "#131317",
-                    },
-                }
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup {
+            color_overrides = {
+                mocha = {
+                    base = "#1c1b22",
+                    crust = "#1c1b22",
+                    mantle = "#131317",
+                },
             }
-            vim.cmd.colorscheme("catppuccin-mocha")
-        end
-    },
-    -- {
-    --     'projekt0n/github-nvim-theme',
-    --     priority = 1000,
-    --
-    -- },
-    -- {
-    --     "folke/tokyonight.nvim",
-    --     priority = 1000,
-    -- },
-    -- {
-    --     "sainnhe/gruvbox-material",
-    --     priority = 1000,
-    --     config = function()
-    --         vim.g.gruvbox_material_palette = "mix"
-    --     end
-    -- },
+        }
+        vim.cmd.colorscheme("catppuccin-mocha")
+    end,
 }
--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -2,9 +2,5 @@ return {
     "saghen/blink.cmp",
     dependencies = "rafamadriz/friendly-snippets",
     version = "*",
-    opts = {
-        sources = {
-            providers = { path = { opts = { get_cwd = function(_) return vim.fn.getcwd() end } } }
-        },
-    },
+    opts = { sources = { providers = { path = { opts = { get_cwd = function(_) return vim.fn.getcwd() end } } } } },
 }
